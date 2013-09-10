2b8b98a8207b8629459eb79bdf280f1f10b807ab
Ankur Srivastava <srivastava.ankur@outlook.com>
2013-09-10 16:07:50 +0530

    initial

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * HelloWorld.java
+ * @author Ankur Srivastava
+ */
+public class HelloWorld {
+  /**
+   * 
+   * @param pMessage the message to be greeted to console 
+   */
+  public void greet(String pMessage) {
+    System.out.println(pMessage);
+  
+ 
+  public static void main(String[] args) {
+    new HelloWorld.greet("Hello"); 
+  }
+}
